9321a9aebc1fb48088b99de40af9a59952131050
inza01-git <facundoinza5@gmail.com>
2023-07-13 11:52:34 -0300

    feat: Config theme provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
+import { lightTheme } from "@/themes";
 import { ThemeProvider } from "@emotion/react";
 import { AppProps } from "next/app";
 
 const Myapp = ({ Component, pageProps }: AppProps) => {
-  return;
-  <ThemeProvider theme={}>
-    <Component {...pageProps} />
-  </ThemeProvider>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default Myapp;
